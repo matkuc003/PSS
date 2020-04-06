3670bab509d1ea3826b3ef40d8f51871aac539b0
mbalcer <mateusz75319@gmail.com>
2020-04-06 21:44:52 +0200

    Created security config

---
+++ src/main/java/com/example/PSO/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.PSO.config;
+
+import com.example.PSO.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().disable();
+        http.authorizeRequests().antMatchers("/api/user/register").permitAll()
+                .and().authorizeRequests().antMatchers("/api/user/all").hasRole("ADMIN")
+                .and().authorizeRequests().antMatchers("/api/delegation/**").authenticated()
+                .and().formLogin().permitAll();
+    }
+}
