47a909fc5cad7c5d70a501adcb2f6b8fe70f0fb5
matkuc003 <mnorris.k97@gmail.com>
2020-04-08 16:33:51 +0200

    Table of Delegations - added - big change

--- src/main/java/com/example/PSO/gui/DelegationView.java
+++ src/main/java/com/example/PSO/gui/DelegationView.java
@@ -6,9 +6,16 @@ import com.example.PSO.models.TransportType;
 import com.example.PSO.models.User;
 import com.example.PSO.service.DelegationService;
 import com.example.PSO.service.UserService;
+import com.vaadin.data.Binder;
+import com.vaadin.data.Binder.Binding;
+import com.vaadin.data.converter.StringToDoubleConverter;
+import com.vaadin.data.converter.StringToIntegerConverter;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.*;
+import com.vaadin.ui.renderers.NumberRenderer;
+import org.w3c.dom.Text;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -27,103 +34,107 @@ public class DelegationView extends HorizontalLayout {
     public void getDelegationView(){
         setSpacing(true);
         setSizeFull();
+
         FormLayout settingFormLayout = new FormLayout();
         HorizontalLayout horizontaLayout = new HorizontalLayout();
         settingFormLayout.setMargin(new MarginInfo(false, true));
 
-        ComboBox<Delegation> delegationsList = new ComboBox<>();
-        delegationsList.setTextInputAllowed(false);
-        delegationsList.setEmptySelectionAllowed(false);
         AtomicReference<List<Delegation>> delegationListToCB = new AtomicReference<>(delegationService.getAllDelByUserOrderByDateStartDesc(loggedUser.getUid()));
-        delegationsList.setItems(delegationListToCB.get());
-        delegationsList.setValue(delegationListToCB.get().get(0));
-        AtomicReference<Delegation> chosenDelegation = new AtomicReference<>(delegationsList.getValue());
 
-        TextField descriptionTextField = new TextField("Description", chosenDelegation.get().getDescription()+"");
-        DateField dateTimeStartDateField = new DateField("dateTimeStart", chosenDelegation.get().getDateTimeStart());
-        DateField dateTimeStopDateField = new DateField("dateTimeStop", chosenDelegation.get().getDateTimeStop());
-        TextField travelDietAmountTextField = new TextField("travelDietAmount", chosenDelegation.get().getTravelDietAmount().toString());
-        TextField breakfastNumberTextField = new TextField("breakfastNumber", chosenDelegation.get().getBreakfastNumber().toString());
-        TextField dinnerNumberTextField = new TextField("dinnerNumber", chosenDelegation.get().getDinnerNumber().toString());
-        TextField supperNumberTextField = new TextField("supperNumber", chosenDelegation.get().getSupperNumber().toString());
+        //Converters
+        StringToIntegerConverter stringToIntConverter = new StringToIntegerConverter("Enter a number!");
+        StringToDoubleConverter stringToDoubleConverter = new StringToDoubleConverter("Enter a double number!");
+
+        //FIELDS IN GRID
+        TextField travelDietAmountTextField = new TextField("travelDietAmount");
+        TextField breakfastNumberTextField = new TextField("breakfastNumber");
+        TextField dinnerNumberTextField = new TextField("dinnerNumber");
+        TextField supperNumberTextField = new TextField("supperNumber");
         ComboBox transportTypeEnum = new ComboBox("transportType");
-
         transportTypeEnum.setItems(TransportType.values());
-        transportTypeEnum.setValue(TransportType.valueOf(chosenDelegation.get().getTransportType().toString()));
-
-        TextField ticketPriceTextField = new TextField("ticketPrice", chosenDelegation.get().getTicketPrice().toString());
-
+        //transportTypeEnum.setValue(TransportType.valueOf(chosenDelegation.get().getTransportType().toString()));
+        TextField ticketPriceTextField = new TextField("ticketPrice");
         ComboBox autoCapacityEnum = new ComboBox("autoCapacity");
         autoCapacityEnum.setItems(AutoCapacity.values());
-        autoCapacityEnum.setValue(chosenDelegation.get().getAutoCapacity());
-        TextField kmTextField = new TextField("km", chosenDelegation.get().getKm().toString());
-        TextField accommodationPriceTextField = new TextField("accommodationPrice", chosenDelegation.get().getAccommodationPrice().toString());
-        TextField otherTicketsPriceTextField = new TextField("otherTicketsPrice", chosenDelegation.get().getOtherTicketsPrice().toString());
-        TextField otherOutlayDescTextField = new TextField("otherOutlayDesc", chosenDelegation.get().getOtherOutlayDesc()+"");
-        TextField otherOutlayPriceTextField = new TextField("otherOutlayPrice", chosenDelegation.get().getOtherTicketsPrice().toString());
+       // autoCapacityEnum.setValue(chosenDelegation.get().getAutoCapacity());
+        TextField kmTextField = new TextField("km");
+        TextField accommodationPriceTextField = new TextField("accommodationPrice");
+        TextField otherTicketsPriceTextField = new TextField("otherTicketsPrice");
+        TextField otherOutlayDescTextField = new TextField("otherOutlayDesc");
+        TextField otherOutlayPriceTextField = new TextField("otherOutlayPrice");
 
-        Button saveDelegation = new Button("Save");
+        //GRID
+        Grid<Delegation> delegationsGrid = new Grid<>();
+        delegationsGrid.getEditor().setEnabled(true);
+        delegationsGrid.setItems(delegationListToCB.get());
+
+        //BINDERS
+        Binding bindTravelDietAmount = delegationsGrid.getEditor().getBinder().forField(travelDietAmountTextField).withConverter(stringToIntConverter).bind(Delegation::getTravelDietAmount,Delegation::setTravelDietAmount);
+        Binding bindBreakFastNumber = delegationsGrid.getEditor().getBinder().forField(breakfastNumberTextField).withConverter(stringToIntConverter).bind(Delegation::getBreakfastNumber,Delegation::setBreakfastNumber);
+        Binding bindDinnerNumber = delegationsGrid.getEditor().getBinder().forField(dinnerNumberTextField).withConverter(stringToIntConverter).bind(Delegation::getDinnerNumber,Delegation::setDinnerNumber);
+        Binding bindSupperNumber = delegationsGrid.getEditor().getBinder().forField(supperNumberTextField).withConverter(stringToIntConverter).bind(Delegation::getSupperNumber,Delegation::setSupperNumber);
+        Binding bindTicketPrice = delegationsGrid.getEditor().getBinder().forField(ticketPriceTextField).withConverter(stringToDoubleConverter).bind(Delegation::getTicketPrice,Delegation::setTicketPrice);
+        Binding bindKM = delegationsGrid.getEditor().getBinder().forField(kmTextField).withConverter(stringToIntConverter).bind(Delegation::getKm,Delegation::setKm);
+        Binding bindAccommodationPrice = delegationsGrid.getEditor().getBinder().forField(accommodationPriceTextField).withConverter(stringToDoubleConverter).bind(Delegation::getAccommodationPrice,Delegation::setAccommodationPrice);
+        Binding bindOtherTicketsPrice = delegationsGrid.getEditor().getBinder().forField(otherTicketsPriceTextField).withConverter(stringToDoubleConverter).bind(Delegation::getOtherTicketsPrice,Delegation::setOtherTicketsPrice);
+        Binding bindOtherOutlayPrice = delegationsGrid.getEditor().getBinder().forField(otherOutlayPriceTextField).withConverter(stringToDoubleConverter).bind(Delegation::getOtherOutlayPrice,Delegation::setOtherOutlayPrice);
+
+        //ADDING COLUMN
+        delegationsGrid.addColumn(Delegation::getDescription).setCaption("Description").setEditorComponent(new TextField(),Delegation::setDescription);
+        delegationsGrid.addColumn(Delegation::getDateTimeStart).setCaption("DateTimeStart").setEditorComponent(new DateField(),Delegation::setDateTimeStart);
+        delegationsGrid.addColumn(Delegation::getDateTimeStop).setCaption("DateTimeStop").setEditorComponent(new DateField(),Delegation::setDateTimeStop);
+        delegationsGrid.addColumn(Delegation::getTravelDietAmount).setCaption("TravelDietAmount").setEditorBinding(bindTravelDietAmount);
+        delegationsGrid.addColumn(Delegation::getBreakfastNumber).setCaption("BreakfastNumber").setEditorBinding(bindBreakFastNumber);
+        delegationsGrid.addColumn(Delegation::getDinnerNumber).setCaption("DinnerNumber").setEditorBinding(bindDinnerNumber);
+        delegationsGrid.addColumn(Delegation::getSupperNumber).setCaption("SupperNumber").setEditorBinding(bindSupperNumber);
+        delegationsGrid.addColumn(Delegation::getTransportType).setCaption("TransportType");
+        delegationsGrid.addColumn(Delegation::getTicketPrice).setCaption("TicketPrice").setEditorBinding(bindTicketPrice);
+        delegationsGrid.addColumn(Delegation::getAutoCapacity).setCaption("AutoCapacity");
+        delegationsGrid.addColumn(Delegation::getKm).setCaption("KM").setEditorBinding(bindKM);
+        delegationsGrid.addColumn(Delegation::getAccommodationPrice).setCaption("AccommodationPrice").setEditorBinding(bindAccommodationPrice);
+        delegationsGrid.addColumn(Delegation::getOtherTicketsPrice).setCaption("OtherTicketsPrice").setEditorBinding(bindOtherTicketsPrice);
+        delegationsGrid.addColumn(Delegation::getOtherOutlayDesc).setCaption("OtherOutlayDesc").setEditorComponent(new TextField(),Delegation::setOtherOutlayDesc);
+        delegationsGrid.addColumn(Delegation::getOtherOutlayPrice).setCaption("OtherOutlayPrice").setEditorBinding(bindOtherOutlayPrice);
+        delegationsGrid.setWidth("100%");
+        delegationsGrid.setSelectionMode(Grid.SelectionMode.MULTI);
+        //SAVE LISTENER
+        delegationsGrid.getEditor().addSaveListener(editorSaveEvent -> {
+
+            delegationService.changeDelegation(editorSaveEvent.getBean().getId(), editorSaveEvent.getBean());
+            delegationsGrid.setItems(delegationService.getAllDelByUserOrderByDateStartDesc(loggedUser.getUid()));
+            delegationsGrid.getDataProvider().refreshAll();
+            Notification.show("Delegation has been saved", Notification.Type.HUMANIZED_MESSAGE);
+        });
+
+
+
+        //DELETE BUTTON
         Button deleteDelegation = new Button("Delete");
-        saveDelegation.addClickListener(clickEvent ->
-        {
-            chosenDelegation.set(delegationsList.getValue());
-
-            chosenDelegation.get().setId(chosenDelegation.get().getId());
-            chosenDelegation.get().setDescription(descriptionTextField.getValue());
-            chosenDelegation.get().setDateTimeStart(dateTimeStartDateField.getValue());
-            chosenDelegation.get().setDateTimeStop(dateTimeStopDateField.getValue());
-            chosenDelegation.get().setTravelDietAmount(Integer.parseInt(travelDietAmountTextField.getValue()));
-            chosenDelegation.get().setBreakfastNumber(Integer.parseInt(breakfastNumberTextField.getValue()));
-            chosenDelegation.get().setDinnerNumber(Integer.parseInt(dinnerNumberTextField.getValue()));
-            chosenDelegation.get().setSupperNumber(Integer.parseInt(supperNumberTextField.getValue()));
-            chosenDelegation.get().setTransportType(TransportType.valueOf(transportTypeEnum.getValue().toString()));
-            chosenDelegation.get().setTicketPrice(Double.parseDouble(ticketPriceTextField.getValue()));
-            chosenDelegation.get().setAutoCapacity(AutoCapacity.valueOf(autoCapacityEnum.getValue().toString()));
-            chosenDelegation.get().setKm(Integer.parseInt(kmTextField.getValue()));
-            chosenDelegation.get().setAccommodationPrice(Double.parseDouble(accommodationPriceTextField.getValue()));
-            chosenDelegation.get().setOtherTicketsPrice(Double.parseDouble(otherTicketsPriceTextField.getValue()));
-            chosenDelegation.get().setOtherOutlayDesc(otherOutlayDescTextField.getValue());
-            chosenDelegation.get().setOtherOutlayPrice(Double.parseDouble(otherOutlayPriceTextField.getValue()));
-
-
-            delegationService.changeDelegation(chosenDelegation.get().getId(), chosenDelegation.get());
-            delegationsList.getDataProvider().refreshAll();
-            Notification.show("All changes has  been saved.", Notification.Type.HUMANIZED_MESSAGE);
-        });
-        delegationsList.addValueChangeListener(delegationValueChangeEvent ->
-        {
-            System.out.println("IMHERE");
-            chosenDelegation.set(delegationValueChangeEvent.getValue());
-
-            descriptionTextField.setValue(chosenDelegation.get().getDescription()+"");
-            dateTimeStartDateField.setValue(chosenDelegation.get().getDateTimeStart());
-            dateTimeStopDateField.setValue(chosenDelegation.get().getDateTimeStop());
-            travelDietAmountTextField.setValue(chosenDelegation.get().getTravelDietAmount().toString());
-            breakfastNumberTextField.setValue(chosenDelegation.get().getBreakfastNumber().toString());
-            dinnerNumberTextField.setValue(chosenDelegation.get().getDinnerNumber().toString());
-            supperNumberTextField.setValue(chosenDelegation.get().getSupperNumber().toString());
-            transportTypeEnum.setValue(chosenDelegation.get().getTransportType().toString());
-            ticketPriceTextField.setValue(chosenDelegation.get().getTicketPrice().toString());
-            autoCapacityEnum.setValue(chosenDelegation.get().getAutoCapacity().toString());
-            kmTextField.setValue(chosenDelegation.get().getKm().toString());
-            accommodationPriceTextField.setValue(chosenDelegation.get().getAccommodationPrice().toString());
-            otherTicketsPriceTextField.setValue(chosenDelegation.get().getOtherTicketsPrice().toString());
-            otherOutlayDescTextField.setValue(chosenDelegation.get().getOtherOutlayDesc()+"");
-            otherOutlayPriceTextField.setValue(chosenDelegation.get().getOtherOutlayPrice().toString());
-        });
 
             deleteDelegation.addClickListener(clickEvent ->
             {
-               delegationService.removeDelegation(loggedUser.getUid(),chosenDelegation.get().getId()); //TODO validation
-               delegationListToCB.get().remove(chosenDelegation);
-                delegationsList.setItems(delegationListToCB.get());
-                delegationsList.getDataProvider().refreshAll();
-                Notification.show("Delegetion has been deleted", Notification.Type.HUMANIZED_MESSAGE);
+              //TODO validation
+                delegationsGrid.getSelectedItems().stream().forEach(d->{
+                    delegationService.removeDelegation(loggedUser.getUid(),d.getId());
+                    delegationListToCB.set(delegationService.getAllDelByUserOrderByDateStartDesc(loggedUser.getUid()));
+                    delegationsGrid.setItems(delegationListToCB.get());
+                    delegationsGrid.getDataProvider().refreshAll();
+                });
+                Notification.show("Delegation has been deleted", Notification.Type.HUMANIZED_MESSAGE);
             });
-        horizontaLayout.addComponents(delegationsList,deleteDelegation);
-        settingFormLayout.addComponents(horizontaLayout,descriptionTextField,dateTimeStartDateField,dateTimeStopDateField,
-                travelDietAmountTextField,breakfastNumberTextField,dinnerNumberTextField,supperNumberTextField,transportTypeEnum,autoCapacityEnum,kmTextField,accommodationPriceTextField,otherTicketsPriceTextField,
-                otherOutlayDescTextField,otherOutlayPriceTextField,saveDelegation);
+            //ADD BUTTON
+        Button addDelegation = new Button("Add New");
+        addDelegation.addClickListener(clickEvent ->
+        {
+            Delegation newDelegation = new Delegation(LocalDate.now(),LocalDate.now().plusDays(1),loggedUser);
+            newDelegation.setDescription("New Delegation - EDIT IT");
+            delegationService.addDelegation(loggedUser.getUid(),newDelegation);
+            delegationsGrid.setItems(delegationService.getAllDelByUserOrderByDateStartDesc(loggedUser.getUid()));
+            delegationsGrid.getDataProvider().refreshAll();
+            Notification.show("Delegation has been added - EDIT IT!", Notification.Type.HUMANIZED_MESSAGE);
+        });
+        horizontaLayout.addComponents(addDelegation,deleteDelegation);
+        settingFormLayout.addComponents(delegationsGrid,horizontaLayout);
         addComponent(settingFormLayout);
     }
 }
